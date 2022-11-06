11f6823a030f6849f22c25a68be8d57c2f4c01d8
wjdgns0664 <wjdgns0664@nate.com>
2022-11-06 13:13:54 +0900

    11.05

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-from pymongo import MongoClient
-import certifi
-
-
 app = Flask(__name__)
-ca = certifi.where()
-client = MongoClient('mongodb+srv://test:123@cluster0.vpw4dwu.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.vpw4dwu.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
 # 메인 페이지 로드
@@ -16,7 +13,28 @@ def home():
 # 자유게시판
 @app.route('/free_board')
 def free_board():
+    comment_receive = request.form['free_board_give']
     return render_template('/free_board.html')
+
+# 자유게시판 포스트
+@app.route('/templates/free_board_review', methods=['POST'])
+def free_board_post():
+    comment_receive = request.form['comment_give']
+    doc = {
+        'comment':comment_receive
+    }
+
+    db.freeboard.insert_one(doc)
+
+    return jsonify({'msg':'저장 완료!'})
+
+# 자유게시판 보여주기
+@app.route('/36Team_ToyProject',methods=["GET"])
+def free_board_get():
+    code_list = list(db.develco_code.find({}, {'_id': False}))
+
+    return jsonify({'codes':code_list})
+
 
 # 코드리뷰 페이지로 이동
 @app.route('/code_review')
