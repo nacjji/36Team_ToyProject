4034e0e0648c63098df53d9608708939ca7f9771
Suyoung Kim <110752019+alan-rla@users.noreply.github.com>
2022-11-09 10:19:23 +0900

    json 파일 파싱 구현 (jsonify -> dumps 변경0

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import certifi
+from bson.json_util import dumps
 
 
 app = Flask(__name__)
@@ -72,12 +73,6 @@ def code_review_post():
     return jsonify({'msg': '업로드 완료!'})
 
 
-
-
-
-
-
-
 # 중고거래 페이지로 이동
 @app.route('/trading')
 def trading():
@@ -86,17 +81,54 @@ def trading():
 # 중고거래 포스트
 @app.route('/templates/trading', methods=['POST'])
 def trading_post():
+    post_receive = request.form['post_give']
+    trading_list = list(db.develco_trading_post.find({}, {'_id': False}))
+    count = len(trading_list) + 1
 
-    return jsonify({'msg':'업로드 완료!'})
+    doc_post = {
+        'num':count,
+        'post':post_receive
+    }
+
+    db.develco_trading_post.insert_one(doc_post)
+
+    doc_comment = {
+        'post_id':list(db.develco_trading_post.find({'num':count},{'_id':True}))[0],
+        'comment': []
+    }
+    db.develco_trading_comment.insert_one(doc_comment)
+
+    return jsonify({'msg':'거래글 작성 완료!'})
+
+# 댓글 포스트
+# @app.route('/templates/trading_comment', methods=['POST'])
+# def comment_post():
+#     comment_receive = request.form['comment_give']
+#     num_receive = request.form['num_give']
+#
+#     posts = list(db.develco_trading_post.find({},{'_id':True}))
+#
+#     for post in posts:
+#         print(post)
+#
+#     doc = {
+#         'num': num_receive,
+#         'comment':comment_receive
+#     }
+#     db.develco_trading_comment.insert_one(doc)
+#
+#     db.develco_trading_comment.update_one(
+#         {'num': num_receive},
+#         {'$push': {'comment': comment_receive}}
+#     )
+#
+#     return jsonify({'msg':'댓글 완료'})
 
 # 중고거래 보여주기
 @app.route('/templates/trading',methods=["GET"])
 def trading_get():
-
-    return jsonify({'msg':'GET 연결 완료!'})
-
-
-
+    trading_list = list(db.develco_trading_post.find({}))
+    return dumps({'trading_posts':trading_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
