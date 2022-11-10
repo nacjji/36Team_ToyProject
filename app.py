fa0ac08eba58f4d009d9264eafd5530a50b78dfd
Suyoung Kim <110752019+alan-rla@users.noreply.github.com>
2022-11-10 17:25:35 +0900

    중고거래 게시글 보여주기

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import certifi
-from bson.json_util import dumps
+from werkzeug.utils import secure_filename
 
+IMAGE_UPLOAD_FOLDER = '/templates/trading_image'
 
 app = Flask(__name__)
 ca = certifi.where()
 client = MongoClient('mongodb+srv://test:123@cluster0.vpw4dwu.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
+app.config['IMAGE_UPLOAD_FOLDER'] = IMAGE_UPLOAD_FOLDER
 
 # 메인 페이지 로드
 @app.route('/')
@@ -82,53 +84,18 @@ def trading():
 @app.route('/templates/trading', methods=['POST'])
 def trading_post():
     post_receive = request.form['post_give']
-    trading_list = list(db.develco_trading_post.find({}, {'_id': False}))
-    count = len(trading_list) + 1
 
-    doc_post = {
-        'num':count,
+    doc = {
         'post':post_receive
     }
-
-    db.develco_trading_post.insert_one(doc_post)
-
-    doc_comment = {
-        'post_id':list(db.develco_trading_post.find({'num':count},{'_id':True}))[0],
-        'comment': []
-    }
-    db.develco_trading_comment.insert_one(doc_comment)
-
+    db.develco_trading_post.insert_one(doc)
     return jsonify({'msg':'거래글 작성 완료!'})
-
-# 댓글 포스트
-# @app.route('/templates/trading_comment', methods=['POST'])
-# def comment_post():
-#     comment_receive = request.form['comment_give']
-#     num_receive = request.form['num_give']
-#
-#     posts = list(db.develco_trading_post.find({},{'_id':True}))
-#
-#     for post in posts:
-#         print(post)
-#
-#     doc = {
-#         'num': num_receive,
-#         'comment':comment_receive
-#     }
-#     db.develco_trading_comment.insert_one(doc)
-#
-#     db.develco_trading_comment.update_one(
-#         {'num': num_receive},
-#         {'$push': {'comment': comment_receive}}
-#     )
-#
-#     return jsonify({'msg':'댓글 완료'})
 
 # 중고거래 보여주기
 @app.route('/templates/trading',methods=["GET"])
 def trading_get():
-    trading_list = list(db.develco_trading_post.find({}))
-    return dumps({'trading_posts':trading_list})
+    trading_list = list(db.develco_trading_post.find({},{'_id':False}))
+    return jsonify({'trading_posts':trading_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
